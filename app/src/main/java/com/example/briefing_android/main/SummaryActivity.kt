089e9812c7b67f60b826ba967f25c3e1063fdd34
nhj7911 <nhj7911@gmail.com>
2021-04-02 16:51:26 +0900

    add Activity

---
+++ app/src/main/java/com/example/briefing_android/main/SummaryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.briefing_android.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.briefing_android.R
+
+class SummaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_summary)
+    }
+}
