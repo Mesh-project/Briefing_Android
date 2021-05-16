14e8d113df85c25ae14e46de6fd717f46d7acba4
user <yenje7@naver.com>
2021-05-16 22:33:58 +0900

    한국어 댓글 전체/긍정 분류

--- app/src/main/java/com/example/briefing_android/summary/fragment_korean.kt
+++ app/src/main/java/com/example/briefing_android/summary/fragment_korean.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +21,8 @@ class fragment_korean : Fragment(){
     private lateinit var  FKrecyclerview : RecyclerView
     private var mpadapter1: rv_Adapter = rv_Adapter(R.layout.comment_item)
     var mp_datalist = ArrayList<ArrayList<CommentItem>>()
+    var commentList = arrayListOf<CommentItem>()
+    var positive_commentList = arrayListOf<CommentItem>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,55 +34,27 @@ class fragment_korean : Fragment(){
         var thiscontext = container!!.getContext()
         FKrecyclerview = korean_listview.findViewById(R.id.korean_recyclerview)
 
-
-//        var commentList = arrayListOf<CommentItem>(
-//            CommentItem("김","2020.01.01","멋지다~",1),
-//            CommentItem("여","2020.01.02","슬프다~",37),
-//            CommentItem("강","2020.03.01","나쁘다~",10000),
-//            CommentItem("나","2020.12.22","이상하다~",777),
-//            CommentItem("오","2020.03.11","좋다~",3),
-//            CommentItem("동","2020.07.23","별로다~",43),
-//            CommentItem("한","2020.09.31","멋지다~",55),
-//            CommentItem("민","2020.01.01","멋지다~",1),
-//            CommentItem("최","2020.01.02","멋지다~",37),
-//            CommentItem("박","2020.03.01","멋지다~",10000),
-//            CommentItem("이","2020.12.22","멋지다~",777),
-//            CommentItem("윤","2020.03.11","좋다~",3),
-//            CommentItem("민","2020.07.23","좋다~",43),
-//            CommentItem("서","2020.09.31","좋다~",55)
-//        )
-
-//        var malicious_commentList = arrayListOf<CommentItem>(
-//            CommentItem("김","2020.01.01","악플~",1),
-//            CommentItem("여","2020.01.02","악플~",37),
-//            CommentItem("강","2020.03.01","악플~",10000),
-//            CommentItem("나","2020.12.22","악플~",777),
-//            CommentItem("오","2020.03.11","악플~",3),
-//            CommentItem("동","2020.07.23","악플~",43),
-//            CommentItem("후","2020.09.31","악플~",55),
-//        )
-
-
-
-//        var btn_filter : Button  = korean_listview.findViewById(R.id.btn_filter)
-//        btn_filter.setOnClickListener(View.OnClickListener {
-//            if(btn_filter.text.toString().equals("ON")){
-//                mpadapter1.data=malicious_commentList
-//                mp_datalist.add(mpadapter1.data)
-//                mpadapter1.notifyDataSetChanged()
-//
-//            }
-//            else{
-//                mpadapter1.data=commentList
-//                mp_datalist.add(mpadapter1.data)
-//                mpadapter1.notifyDataSetChanged()
-//
-//            }
-//
-//        })
-
         server(thiscontext)
 
+        var flag=0
+        var btn_filter : Button = korean_listview.findViewById(R.id.btn_filter)
+        btn_filter.setOnClickListener(View.OnClickListener {
+            if(btn_filter.text.toString().equals("ON")){
+                if(flag==0){
+                    positive_server(thiscontext)
+                    flag=1
+                }
+                else{
+                    mpadapter1.data=positive_commentList
+                    mpadapter1.notifyDataSetChanged()
+                }
+            }
+            else{
+                mpadapter1.data=commentList
+                mpadapter1.notifyDataSetChanged()
+            }
+
+        })
 
 
         return korean_listview
@@ -91,7 +66,7 @@ class fragment_korean : Fragment(){
         val callcommentpost = UserServiceImpl.CommentService.requestURL(CommentURLRequest = CommentURLRequest("V1WHgI2xM2k"))
         callcommentpost.safeEnqueue {
             if(it.isSuccessful){
-                var commentList = arrayListOf<CommentItem>()
+
                 val korean_CommentList = it.body()!!.korean_data
                 for(i in 0 until korean_CommentList.size){
                     commentList.add(
@@ -102,22 +77,54 @@ class fragment_korean : Fragment(){
                             it_likecount=korean_CommentList[i].likecount
                         )
                     )
+
+
                 }
-                Log.v("111","111")
+
                 //리사이클러뷰의 어댑터 세팅
                 FKrecyclerview.adapter=mpadapter1
-
                 //리사이클러뷰 배치
                 val lm = LinearLayoutManager(thiscontext)
                 FKrecyclerview.layoutManager=lm
-                Log.v("222","222")
                 mpadapter1.data=commentList
                 mpadapter1.notifyDataSetChanged()
                 mp_datalist.add(mpadapter1.data)
-                Log.v("333","333")
+
             }
         }
         mpadapter1.notifyDataSetChanged()
     }
 
+    private fun positive_server(thiscontext: Context){
+        mpadapter1.notifyDataSetChanged()
+
+        val callcommentpost = UserServiceImpl.CommentService.requestURL(CommentURLRequest = CommentURLRequest("V1WHgI2xM2k"))
+        callcommentpost.safeEnqueue {
+            if(it.isSuccessful) {
+                val korean_positive_CommentList = it.body()!!.korean_data
+                for (i in 0 until korean_positive_CommentList.size) {
+                    if (korean_positive_CommentList[i].predict.substring(11, 13).equals("긍정")) {
+                        positive_commentList.add(
+                            CommentItem(
+                                it_username = korean_positive_CommentList[i].nickname,
+                                it_date = korean_positive_CommentList[i].writetime.substring(0, 10),
+                                it_comment = korean_positive_CommentList[i].comment,
+                                it_likecount = korean_positive_CommentList[i].likecount
+                            )
+                        )
+                    }
+                }
+                //리사이클러뷰의 어댑터 세팅
+                FKrecyclerview.adapter = mpadapter1
+
+                //리사이클러뷰 배치
+                val lm = LinearLayoutManager(thiscontext)
+                FKrecyclerview.layoutManager = lm
+                mpadapter1.data = positive_commentList
+                mpadapter1.notifyDataSetChanged()
+                mp_datalist.add(mpadapter1.data)
+            }
+        }
+        mpadapter1.notifyDataSetChanged()
+    }
 }
